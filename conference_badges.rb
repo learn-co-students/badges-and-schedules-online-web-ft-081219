33f092d4181cf7da5d1205a922dde23df64df173
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-15 07:00:38 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(name)
+  new_list = []
+  name.each do |list| 
+     new_list.push("Hello, my name is #{list}.")
+  end
+  return new_list
+end
+
+def assign_rooms(name)
+    rooms_assigments = []
+    
+    
+    name.each do |list|
+      
+      
+      rooms_assigments.push( "Hello, #{list}! You'll be assigned to room #{room}!")
+      
+    end
+    
+    return rooms_assigments
+end
+  
+def printer 
+  
+end
